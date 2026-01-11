fe45d23513b95aadc653f26d45d8044dbc37346d
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-01-12 01:03:42 +0530

    finding area and volume of cube

---
+++ Basics/AreaVolumeofcube.java
@@ -0,0 +1,25 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class AreaVolumeofcube {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+         int length,breadth,height;
+         int totalArea,volume;
+
+         System.out.println("enter length breadth and height");
+         length = sc.nextInt();
+         breadth =sc.nextInt();
+         height=sc.nextInt();
+
+         totalArea= 2*(length*breadth+length*height+breadth*height);
+         volume=length*breadth*height;
+
+         System.out.println("Total AREA "+totalArea);
+         System.out.println("volume "+volume);
+
+         sc.close();
+    }
+    
+}
