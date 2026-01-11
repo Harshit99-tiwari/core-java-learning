357fcd324a6a030fb558fd0a0cdf17169d39131a
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-01-12 01:07:50 +0530

    printing size of diff data types

---
+++ Basics/datatypesize.java
@@ -0,0 +1,32 @@
+package Basics;
+
+public class datatypesize {
+    public static void main(String[] args) {
+
+        System.out.println("float Min:" + Float.MIN_VALUE);
+        System.out.println("float Max:" + Float.MAX_VALUE);
+        System.out.println("float Bytes " + Float.SIZE);
+
+        System.out.println("double Min:" + Double.MIN_VALUE);
+        System.out.println("double Max:" + Double.MAX_VALUE);
+        System.out.println("double Bytes " + Double.SIZE);
+
+        System.out.println("long Min:" + Long.MIN_VALUE);
+        System.out.println("long Max:" + Long.MAX_VALUE);
+        System.out.println("long Bytes " + Long.SIZE);
+
+        System.out.println("short Min:" + Short.MIN_VALUE);
+        System.out.println("shortMax:" + Short.MAX_VALUE);
+        System.out.println("shortBytes " + Short.SIZE);
+
+        System.out.println("Byte Min:" + Byte.MIN_VALUE);
+        System.out.println("Byte Max:" + Byte.MAX_VALUE);
+        System.out.println("Byte Bytes " + Byte.SIZE);
+
+        System.out.println("Int Min:" + Integer.MIN_VALUE);
+        System.out.println("Int Max:" + Integer.MAX_VALUE);
+        System.out.println("Int Bytes " + Integer.SIZE);
+
+    }
+
+}
