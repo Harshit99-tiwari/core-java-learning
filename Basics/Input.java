f8ddaffe8a2c15e2c53e240204ac7bbc167cf9aa
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-01-11 18:13:50 +0530

    taking input by user

--- Basics/Input.java
+++ Basics/Input.java
@@ -1,5 +1,17 @@
 package Basics;
 
+import java.util.Scanner;
+
 public class Input {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Your name: "+name);
+        System.out.print("Enter your age:");
+        int age = sc.nextInt();
+        System.out.println("Your age: "+age);
+
+        sc.close();
+    }
 }
