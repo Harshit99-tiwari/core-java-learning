2144a81351f076232cb0e21e9d0b278c8004ac01
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-01-13 17:21:58 +0530

    add

--- Basics/MyFirst.java
+++ Basics/MyFirst.java
@@ -3,5 +3,8 @@ package Basics;
 public class MyFirst {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int a=5;
+        int b=8;
+        System.out.println((a+b));
     }
 }
