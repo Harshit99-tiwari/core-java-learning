c0f91aed5c0162f064b4ff3ffed22d56a7d89b10
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-01-13 22:32:31 +0530

    simple condition practice

---
+++ Conditional_Statements/IfandElse1.java
@@ -0,0 +1,13 @@
+package Conditional_Statements;
+
+public class IfandElse1 {
+    public static void main(String[] args) {
+         int x= -9;
+          if(x>=0){
+          System.out.println("positive");
+          }
+          else{
+          System.out.println("negative");
+          }
+    }
+}
