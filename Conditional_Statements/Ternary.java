f5919b3ebc383f7e52b0e5afef7e3ca3efa7c592
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-01-14 20:39:12 +0530

    ternary operator condition

---
+++ Conditional_Statements/Ternary.java
@@ -0,0 +1,10 @@
+package Conditional_Statements;
+
+public class Ternary {
+    public static void main(String[] args) {
+        int age = 19;
+        char status;
+        status = age <= 100 ? 'v' : 'w';
+        System.out.println(status);
+    }
+}
