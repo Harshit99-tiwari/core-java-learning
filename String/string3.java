11d1ed7b607f6567c8c332df34dd24f3f94d63b9
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-01-16 15:34:50 +0530

    printing string length and making string with chars

---
+++ String/string3.java
@@ -0,0 +1,13 @@
+package String;
+
+public class string3 {
+    public static void main(String[] args) {
+        char c[] = { 'h', 'e', 'l', 'l', 'o' };
+        String str5 = new String(c, 1, 4);
+        System.out.println(str5);
+
+        String str = "java";
+        int l = str.length();
+        System.out.println(l);
+    }
+}
