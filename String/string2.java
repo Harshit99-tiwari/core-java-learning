939948585354bb418504a09889a152aac4954800
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-01-16 15:30:28 +0530

    basic string printing

---
+++ String/string2.java
@@ -0,0 +1,12 @@
+package String;
+
+public class string2 {
+    public static void main(String[] args) {
+        byte b[] = { 65, 66, 67, 68 };
+        String str2 = new String(b);
+        System.out.println(str2);
+
+        String str3 = new String("java");
+        System.out.println(str3);
+    }
+}
