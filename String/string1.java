8ddf9ba87c644a78cd814ccde9a1c3e39fba745d
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-01-16 15:26:50 +0530

    making string with group of characters

---
+++ String/string1.java
@@ -0,0 +1,10 @@
+package String;
+
+public class string1 {
+    public static void main(String[] args) {
+          char c[]={'A','B','C','D'};
+         String str1 = new String(c);
+        System.out.println(str1);
+
+    }
+}
