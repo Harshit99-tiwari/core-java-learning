418a34c411e90394bfe5425ec0cfb9649e2b9f53
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-01-16 15:41:44 +0530

    converting string to lowercase and uppercase

---
+++ String/stringmethod1.java
@@ -0,0 +1,13 @@
+package String;
+
+public class stringmethod1 {
+    public static void main(String[] args) {
+         String str="JAVA";    //converting to lowercase
+          str=str.toLowerCase();
+          System.out.println(str);
+
+          String str1="java";
+          str1=str1.toUpperCase();
+          System.out.println(str1);
+    }
+}
