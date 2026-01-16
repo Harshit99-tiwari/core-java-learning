9d278f4aa6fecbf3cd141d19d81e7a7b59304b71
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-01-16 15:36:55 +0530

    comparing two strings

---
+++ String/string4.java
@@ -0,0 +1,11 @@
+package String;
+
+public class string4 {
+    public static void main(String[] args) {
+        String str1="java";
+          String str2="java";
+          System.out.println(str1==str2);
+          
+        
+    }
+}
