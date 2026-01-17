d21253b8db35bd95cb322f4023d571d44f7722f8
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-01-17 17:20:49 +0530

    implementing indexof and lastIndexof method

---
+++ String/stringmethod5.java
@@ -0,0 +1,12 @@
+package String;
+
+public class stringmethod5 {
+    public static void main(String[] args) {
+        String str = "www.udemy.co.in";
+        System.out.println(str.indexOf(".", 4));
+        System.out.println(str.lastIndexOf("."));
+        System.out.println(str.indexOf("udemy"));
+        System.out.println(str.indexOf('u'));
+
+    }
+}
