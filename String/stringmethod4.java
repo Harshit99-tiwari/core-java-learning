71095a248a96e90c1cac926e5d807e21fcb699c8
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-01-17 17:17:31 +0530

    implementing startswith,endswith,charat methods

---
+++ String/stringmethod4.java
@@ -0,0 +1,10 @@
+package String;
+
+public class stringmethod4 {
+    public static void main(String[] args) {
+        String str = "Mr. Shahrukh khan";
+        System.out.println(str.startsWith("shah"));
+        System.out.println(str.endsWith("khan"));
+        System.out.println(str.charAt(4));
+    }
+}
