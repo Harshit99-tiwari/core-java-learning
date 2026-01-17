921020cfd6f91be44e3c0200b66c81f70860ec5b
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-01-17 17:07:14 +0530

    triming string and replacing character in string

---
+++ String/stringmethod2.java
@@ -0,0 +1,11 @@
+package String;
+
+public class stringmethod2 {
+    public static void main(String[] args) {
+        String str = "    welcome  ";
+        System.out.println(str.trim());
+
+        String str1 = "Harsh";
+        System.out.println(str1.replace('H', 'W'));
+    }
+}
