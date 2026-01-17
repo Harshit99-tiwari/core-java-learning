3788829c535f75eeeb501ce7efdb45126ca9e549
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-01-17 17:25:03 +0530

    implementing equals and equalsIgnorecase methods

---
+++ String/stringmethod6.java
@@ -0,0 +1,17 @@
+package String;
+
+public class stringmethod6 {
+    public static void main(String[] args) {
+         String str2="java";
+           String str3="python";
+           String str4="python";
+           System.out.println(str3.equals(str4));
+           System.out.println( str2.equals(str4));
+           
+           String str1 = "Pyramid";
+           String str11 = "pyramid";
+           System.out.println(str1.equalsIgnoreCase(str11));
+
+          
+    }
+}
