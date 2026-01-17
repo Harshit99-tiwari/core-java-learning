5e926594292e7f82ea4645010d6989d1e9ed7aca
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-01-17 17:14:25 +0530

    implementing substring method

---
+++ String/stringmethod3.java
@@ -0,0 +1,10 @@
+package String;
+
+public class stringmethod3 {
+    public static void main(String[] args) {
+        String str = "welcome" ;
+        System.out.println(str.substring(2));
+        System.out.println(str.substring(1, 4));
+        System.out.println(str.substring(1,str.length()));
+    }
+}
