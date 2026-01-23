79bc1056c71118d9d47e23365118dfeeb34caa89
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-01-23 23:26:11 +0530

    Traversing the array

---
+++ Arrays/demo.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class demo {
+    public static void main(String[] args) {
+        int A[]={1,2,3,4,5};
+    
+        for(int i =0;i<A.length;i++){
+            System.out.print(A[i]+" ");
+        }  
+    }
+}
