a406fd2b6d7bc6e799a0014554f319247fee732b
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-01-31 11:35:30 +0530

    counting digit of a number

--- Loops/Digitcount.java
+++ Loops/Digitcount.java
@@ -7,14 +7,14 @@ public class Digitcount {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number");
         int n = sc.nextInt();
+        int sum = 0;
         int count = 0;
         while (n > 0) {
             n = n / 10;
+            sum = sum +n;
             count++;
-
         }
         System.out.println(count);
         sc.close();
-
     }
 }
