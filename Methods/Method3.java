48a4303ff7f3a7b489a8f36a791a7060077c9e03
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-02-07 21:03:14 +0530

    practicing method

---
+++ Methods/Method3.java
@@ -0,0 +1,12 @@
+package Methods;
+
+public class Method3 {
+    static void welcome(String n) {
+        System.out.println("welcome mr. " + n);
+    }
+
+    public static void main(String[] args) {
+        String name = "harshit";
+        welcome(name);
+    }
+}
