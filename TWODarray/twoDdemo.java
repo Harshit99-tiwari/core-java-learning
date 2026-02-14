4074e1febda0b2a4e1cb48ff1e7234bd9dae1e8e
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-02-14 14:58:25 +0530

    declaratrion of 2D array

---
+++ TWODarray/twoDdemo.java
@@ -0,0 +1,18 @@
+package TWODarray;
+
+public class twoDdemo {
+    public static void main(String[] args) {
+        int A[][]= new int[5][5];
+        A[0][2]=1;
+        
+       // int B[][] = {{1,2,3},{2,3,4}};
+
+        //int c[][];
+       // c = new int[4][4];
+
+        int[] e,f[];
+        e = new int[5];
+        f = new int[4][3];
+
+    }
+}
