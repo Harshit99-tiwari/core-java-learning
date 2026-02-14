b3cb3322e39e9aee800696e7c367888dd18604d9
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-02-14 15:55:55 +0530

    traversing 2D array

---
+++ TWODarray/Traverse2D.java
@@ -0,0 +1,14 @@
+package TWODarray;
+
+public class Traverse2D {
+    public static void main(String[] args) {
+       int A[][]={{1,2,3,4},{5,6,7,8},{9,2,3,4},{2,3,4,5}};
+       //PRINTING ALL ELEMENT OF 2D ARRAY
+        for(int i =0;i<A.length;i++){
+         for(int j =0;j<A[0].length;j++){
+            System.out.print(A[i][j]+" ");
+       }
+       System.out.println("\n");
+    }  
+    }
+}
