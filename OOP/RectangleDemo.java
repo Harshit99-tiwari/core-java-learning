95aee3fa5158a97b582092a648565b19aa5677d4
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-02-21 15:58:27 +0530

    Enhance Rectangle class with validation,area,perimeter and isSquare methods

---
+++ OOP/RectangleDemo.java
@@ -0,0 +1,71 @@
+package OOP;
+
+public class RectangleDemo {
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle(10, 10);
+        System.out.println("Area: " + r.area());
+        System.out.println("Perimeter: " + r.perimeter());
+        System.out.println("is this square: " + r.isSquare());
+    }
+}
+
+class Rectangle {
+    private double length;
+    private double breadth;
+
+    public Rectangle() {
+        length = 1;
+        breadth = 1;
+    }
+
+    public Rectangle(double l, double b) {
+        setLength(l);
+        setBreadth(b);
+    }
+
+    public Rectangle(double s) {
+        length = breadth = s;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getBreadth() {
+        return breadth;
+    }
+
+    public void setLength(double l) {
+        if (l >= 0) {
+            length = l;
+        } else {
+            length = 0;
+        }
+    }
+
+    public void setBreadth(double b) {
+        if (b >= 0) {
+            breadth = b;
+        } else {
+            breadth = 0;
+        }
+
+    }
+
+    public double area() {
+        return getLength() * getBreadth();
+    }
+
+    public double perimeter() {
+        return 2 * (length + breadth);
+    }
+
+    public boolean isSquare() {
+        if (length == breadth) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
