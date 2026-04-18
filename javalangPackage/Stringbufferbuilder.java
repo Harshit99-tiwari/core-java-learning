7f050f2c61b9f231bdc07b722e0f010497398f19
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-04-18 21:39:10 +0530

    string buffer and builder

---
+++ javalangPackage/Stringbufferbuilder.java
@@ -0,0 +1,15 @@
+package javalangPackage;
+
+public class Stringbufferbuilder {
+    public static void main(String[] args) {
+        String s1=new String("Hello");
+        StringBuffer s2=new StringBuffer("Hello");
+        StringBuilder s3=new StringBuilder("Hello");
+
+        s1.concat("world");
+        s2.append("world");
+        s3.append("world");
+        System.out.println(s1+" "+s2+" "+s3);
+
+    }
+}
