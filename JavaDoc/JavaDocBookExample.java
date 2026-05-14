6cc1e31ff8112df4ac0b98a0adb56af9af4e77f8
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-05-14 22:49:15 +0530

    Added JavaDoc documentation example for Book class methods and constructors

---
+++ JavaDoc/JavaDocBookExample.java
@@ -0,0 +1,47 @@
+/** @author Harshit tiwari
+ * @version 1.0
+ * @since 2026
+ * 
+ **/
+package JavaDoc;
+/**  @author Harshit tiwari
+ *   Class for Library book 
+ * 
+ * 
+ **/
+
+public class JavaDocBookExample {
+      /**
+     * value 10 default value
+     */
+    static int val = 10;
+
+    /**
+     * parameterized constructor
+     * @param s Book name
+     */
+    public JavaDocBookExample(String s){}
+
+    /**
+     * Issue a Book to a student
+     * @param roll roll no of a student
+     * @throws Exception if book is not available,throws exception
+     */
+          /**
+     * check if book is available
+     * @param str book name
+     * @return if book is available return true else false
+     */
+    public boolean available(String str){
+        return true;
+    }
+    /**
+     * Get book name
+     * @param id book id
+     * @return  return book name
+     */
+    public String getname(int id){
+        return "";
+    } 
+    }
+
