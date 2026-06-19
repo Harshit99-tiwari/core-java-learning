d84831d8aae4443b9e2dc3fed3d24cc185616215
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-06-19 20:00:39 +0530

    Add AWT mouse event handling demo using MouseListener and MouseMotionListener

---
+++ AWT/MouseEventDemo.java
@@ -0,0 +1,9 @@
+package AWT;
+
+public class MouseEventDemo {
+     public static void main(String[] args) {
+        MMFrame f = new MMFrame();
+        f.setSize(400,400);
+        f.setVisible(true);
+    }
+}
